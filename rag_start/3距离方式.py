30b1687a70599d34fafcc1940ace0fe9d1dd2815
刘春达 <liuchunda@xiaomi.com>
2026-08-07 18:34:38 +0800

    feat: 距离方式

---
+++ rag_start/3距离方式.py
@@ -0,0 +1,13 @@
+# metric_type: 距离计算方式L2、IP、COSINE
+# index_type: 索引类型FLAT、IVF_FLAT、IVF_PQ、HNSW、ANNOY
+# params: 索引参数，如nlist、M、efconstruction、m
+
+# L2: 欧氏距离
+# IP: 内积
+# COSINE: 余弦距离
+
+# FLAT: 暴力搜索
+# IVF_FLAT: 倒排文件
+# IVF_PQ: 倒排文件+量化
+# HNSW: 多层近邻图
+# ANNOY: 树状索引
